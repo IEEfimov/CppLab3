401075d55115b2c0e51b9cc48f24b7b5072924bc
IEEfimov <vanchick.fimchick15@gmail.com>
2018-11-16 05:17:01 +0200

    task 1 has been done

---
+++ tasks/1/run.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\MPICH2\bin\mpiexec.exe" -np 1 "CppLab3.exe" > out1.txt
+"C:\Program Files (x86)\MPICH2\bin\mpiexec.exe" -np 2 "CppLab3.exe" > out2.txt
+"C:\Program Files (x86)\MPICH2\bin\mpiexec.exe" -np 3 "CppLab3.exe" > out3.txt
+"C:\Program Files (x86)\MPICH2\bin\mpiexec.exe" -np 4 "CppLab3.exe" > out4.txt
